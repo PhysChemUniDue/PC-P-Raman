0aff07145717302fdbcd98adce7d79440dece967
lambcount <53368935+lambcount@users.noreply.github.com>
2025-05-16 14:05:18 +0200

    Referenzspektren in Marimo Chemometrics integriert.

--- Python/Chemometrics.py
+++ Python/Chemometrics.py
@@ -14,7 +14,8 @@ def _():
     from ipywidgets import interact, FloatSlider
     from sklearn.linear_model import HuberRegressor
     from scipy.optimize import nnls
-    return HuberRegressor, go, make_subplots, mo, nnls, np, random
+    import os 
+    return HuberRegressor, go, make_subplots, mo, nnls, np, os, random
 
 
 @app.cell(hide_code=True)
@@ -38,80 +39,120 @@ def _(np):
             y += lorentzian(x, center, width, amp)
         return y
 
-    return raman_spectrum, x
+    return
 
 
 @app.cell(hide_code=True)
-def _(raman_spectrum, random, x):
-    # Define three example spectra with different sets of bands (center, )
-    bands_list = [
-        [(218, 10, 1.0), (288, 10, 0.4), (401, 10, 1.3), (602, 10, 0.5), (1200, 10, 0.8), (2300, 10, 0.5)],
-        [(193, 10, 0.2), (306, 10, 0.7), (538, 10, 1.3), (668, 10, 0.5), (1157, 10, 1.4), (2323, 10, 0.8)],
-        [(180, 10, 0.7), (267, 10, 0.6), (451, 10, 1.0), (558, 10, 0.6), (1254, 10, 1.0), (2215, 10, 0.4)],
-    ]
-    spectra = [raman_spectrum(x, bands) for bands in bands_list]
+def _(norm, np, os):
+    folder = '../Data/Literatur/'
+    hematite  = np.loadtxt(os.path.join(folder, 'Hämatit - Fe2O3'     ,'fe2o3-processed.txt'  ),delimiter=',', skiprows=10)
+    magnetite = np.loadtxt(os.path.join(folder, 'Magnetit-Fe3O4'      ,'fe3o4-processed.txt'  ),delimiter=',', skiprows=10)
+    goethite  = np.loadtxt(os.path.join(folder, 'Goethit - a-FeOOH'   ,'a-feooh-raw.txt'),delimiter=',', skiprows=10)
+    lepido    = np.loadtxt(os.path.join(folder, 'Lepidokrit - g-FeOOH','g-feooh-raw.txt'),delimiter=',', skiprows=10)
+
+    # 1. Gemeinsamen x-Bereich bestimmen (Schnittmenge aller Intervalle, um Extrapolation zu vermeiden)
+    x_min = max(hematite[:,0].min(),
+                magnetite[:,0].min(),
+                goethite[:,0].min(),
+                lepido[:,0].min())
+    x_max = min(hematite[:,0].max(),
+                magnetite[:,0].max(),
+                goethite[:,0].max(),
+                lepido[:,0].max())
+
+    # 2. Gemeinsamen x-Vektor erzeugen (z.B. 1000 Punkte)
+    common_x = np.linspace(x_min, x_max, 1000)
+
+    # 3. Mittels np.interp auf den gemeinsamen Vektor routen
+    y_hem   = norm(np.interp(common_x, hematite[:,0],  hematite[:,1]))
+    y_mag   = norm(np.interp(common_x, magnetite[:,0], magnetite[:,1]))
+    y_goet  = norm(np.interp(common_x, goethite[:,0],  goethite[:,1]))
+    y_lepid = norm(np.interp(common_x, lepido[:,0],    lepido[:,1]))
+
+    return common_x, y_goet, y_hem, y_lepid, y_mag
+
+
+@app.cell(hide_code=True)
+def _(np):
+    def norm(A):
+        return A /np.max(A)
+
+    
+    return (norm,)
+
+
+@app.cell(hide_code=True)
+def _(random, y_goet, y_hem, y_lepid, y_mag):
+    spectra = [y_hem,y_mag,y_goet,y_lepid]
 
     # Random Konvolution der Spektren 
     a = round(random.uniform(0.01, 10.0), 2)
     b = round(random.uniform(0.01, 10.0), 2)
     c = round(random.uniform(0.01, 10.0), 2)
+    d = round(random.uniform(0.01, 10.0), 2)
 
-    per_a = a/ (a+b+c)*100
-    per_b = b/ (a+b+c)*100
-    per_c = c/ (a+b+c)*100
-    y_test = a * spectra[0] + b * spectra[1] +c * spectra[2]
-    return per_a, per_b, per_c, spectra, y_test
+    per_a = a/ (a+b+c+d) * 100 
+    per_b = b/ (a+b+c+d) * 100 
+    per_c = c/ (a+b+c+d) * 100 
+    per_d = d/ (a+b+c+d) * 100 
+
+    y_test = a * spectra[0] + b * spectra[1] +c * spectra[2] + d * spectra[3]
+    return per_a, per_b, per_c, per_d, spectra, y_test
 
 
 @app.cell(hide_code=True)
 def _(mo):
-
     # Create sliders using Marimo's slider API (0–10)
     s1 = mo.ui.slider(
-        label="Spektrum A", start=0, stop=10, step=0.01, value=1,
+        label="Hämatit", start=0, stop=10, step=0.01, value=1,
     )
     s2 = mo.ui.slider(
-        label="Spektrum B", start=0, stop=10, step=0.01, value=1,
+        label="Magnetit", start=0, stop=10, step=0.01, value=1,
     )
     s3 = mo.ui.slider(
-        label="Spektrum C", start=0, stop=10, step=0.01, value=1,
+        label="Goethit", start=0, stop=10, step=0.01, value=1,
+    )
+    s4 = mo.ui.slider(
+        label="Lepidokrit", start=0, stop=10, step=0.01, value=1,
     )
     # Arrange sliders horizontally
-    mo.hstack([s1, s2, s3])
+    mo.hstack([s1, s2, s3,s4])
 
-    return s1, s2, s3
+    return s1, s2, s3, s4
 
 
 @app.cell(hide_code=True)
-def _(HuberRegressor, nnls, np, s1, s2, s3, spectra, y_test):
+def _(HuberRegressor, nnls, np, s1, s2, s3, s4, spectra, y_test):
     # Spektren mit Faktor
     y1 = s1.value * spectra[0]
     y2 = s2.value * spectra[1]
     y3 = s3.value * spectra[2]
+    y4 = s4.value * spectra[3]
     # Kombination 
-    y_combined = y1+y2+y3
+    y_combined = y1+y2+y3+y4
     y_residual = y_test - y_combined
 
     # Designmatrix wie oben
-    X = np.vstack([spectra[0], spectra[1], spectra[2]]).T
+    X = np.vstack([spectra[0], spectra[1], spectra[2], spectra[3]]).T
     y = y_test
 
     # Huber-Regressor (default epsilon=1.35)
     model = HuberRegressor(fit_intercept=False)
     model.fit(X, y)
-    a_huber, b_huber, c_huber = model.coef_
-    per_a_huber = a_huber/(a_huber+b_huber+ c_huber ) * 100
-    per_b_huber = b_huber/(a_huber+b_huber+ c_huber ) * 100
-    per_c_huber = c_huber/(a_huber+b_huber+ c_huber ) * 100
-
+    a_huber, b_huber, c_huber,d_huber = model.coef_
+    per_a_huber = a_huber/(a_huber+b_huber+ c_huber + d_huber) * 100
+    per_b_huber = b_huber/(a_huber+b_huber+ c_huber + d_huber) * 100
+    per_c_huber = c_huber/(a_huber+b_huber+ c_huber + d_huber) * 100
+    per_d_huber = d_huber/(a_huber+b_huber+ c_huber + d_huber) * 100
 
     y_fit_huber = model.predict(X)
     # NNLS lösen
     coeffs, rnorm = nnls(X, y)
-    a_nnls, b_nnls, c_nnls = coeffs
-    per_a_nnls = a_nnls/(a_nnls+b_nnls+ c_nnls ) * 100
-    per_b_nnls = b_nnls/(a_nnls+b_nnls+ c_nnls ) * 100
-    per_c_nnls = c_nnls/(a_nnls+b_nnls+ c_nnls ) * 100
+    a_nnls, b_nnls, c_nnls,d_nnls = coeffs
+    per_a_nnls = a_nnls/(a_nnls+b_nnls+ c_nnls +d_nnls) * 100
+    per_b_nnls = b_nnls/(a_nnls+b_nnls+ c_nnls +d_nnls) * 100
+    per_c_nnls = c_nnls/(a_nnls+b_nnls+ c_nnls +d_nnls) * 100
+    per_d_nnls = d_nnls/(a_nnls+b_nnls+ c_nnls +d_nnls) * 100
 
 
     # Gefittetes Spektrum rekonstruieren
@@ -125,6 +166,7 @@ def _(HuberRegressor, nnls, np, s1, s2, s3, spectra, y_test):
         y1,
         y2,
         y3,
+        y4,
         y_combined,
         y_fit_huber,
         y_residual,
@@ -134,6 +176,7 @@ def _(HuberRegressor, nnls, np, s1, s2, s3, spectra, y_test):
 
 @app.cell(hide_code=True)
 def _(
+    common_x,
     go,
     make_subplots,
     mo,
@@ -143,17 +186,26 @@ def _(
     per_b_nnls,
     per_c,
     per_c_nnls,
-    x,
+    per_d,
+    s1,
+    s2,
+    s3,
+    s4,
     y1,
     y2,
     y3,
+    y4,
     y_combined,
     y_fit_huber,
     y_residual,
     y_residual_fit,
     y_test,
 ):
-    # Set up subplots: 2 rows, shared x-axis, with different heights
+    s_a = s1.value/(s1.value+s2.value+s3.value+s4.value)*100
+    s_b = s2.value/(s1.value+s2.value+s3.value+s4.value)*100
+    s_c = s3.value/(s1.value+s2.value+s3.value+s4.value)*100
+    s_d = s4.value/(s1.value+s2.value+s3.value+s4.value)*100
+
     fig = make_subplots(
         rows=2, cols=1,
         shared_xaxes=True,
@@ -163,31 +215,36 @@ def _(
 
     # Main plot traces in first row
     fig.add_trace(go.Scatter(
-        x=x, y=y1, mode='lines', name='Spektrum A',
+        x=common_x, y=y1, mode='lines', name='Hämatit',
         line=dict(color='green', width=2, dash='dash'), opacity=0.8
     ), row=1, col=1)
     fig.add_trace(go.Scatter(
-        x=x, y=y2, mode='lines', name='Spektrum B',
-        line=dict(color='orange', width=2, dash='dash'), opacity=0.8
+        x=common_x, y=y2, mode='lines', name='Magnetit',
+        line=dict(color='coral', width=2, dash='dash'), opacity=0.8
     ), row=1, col=1)
     fig.add_trace(go.Scatter(
-        x=x, y=y3, mode='lines', name='Spektrum C',
-        line=dict(color='purple', width=2, dash='dash'), opacity=0.8
+        x=common_x, y=y3, mode='lines', name='Goethit',
+        line=dict(color='steelblue', width=2, dash='dash'), opacity=0.8
     ), row=1, col=1)
+    fig.add_trace(go.Scatter(
+        x=common_x, y=y4, mode='lines', name='Lepidokrit',
+        line=dict(color='grey', width=2, dash='dash'), opacity=0.8
+    ), row=1, col=1)
+
     # Combined spectrum 
-    fig.add_trace(go.Scatter(x=x, y=y_combined, mode='lines', name='Kombiniertes Slider-Spektrum',
+    fig.add_trace(go.Scatter(x=common_x, y=y_combined, mode='lines', name=f'Kombiniertes Slider-Spektrum (A={s_a:.2f}, B={s_b:.2f}, C={s_c:.2f}, D={s_d:.2f})',
                              line=dict(color='black', width=2)), row=1, col=1)
     # Test spectrum 
-    fig.add_trace(go.Scatter(x=x, y=y_test, mode='lines', name=f'Test-Spektrum (A={per_a:.2f}, B={per_b:.2f}, C={per_c:.2f})',
+    fig.add_trace(go.Scatter(x=common_x, y=y_test, mode='lines', name=f'Test-Spektrum (A={per_a:.2f}, B={per_b:.2f}, C={per_c:.2f}, D={per_d:.2f})',
                              line=dict(color='red', width=2)), row=1, col=1)
     # Fit 
 
-    fig.add_trace(go.Scatter(x=x, y=y_fit_huber, mode='lines', name=f'Fitted (A={per_a_nnls:.2f}), B={per_b_nnls:.2f}), C={per_c_nnls:.2f})', line=dict(color='blue', width=2)), row=1, col=1)
+    fig.add_trace(go.Scatter(x=common_x, y=y_fit_huber, mode='lines', name=f'Fitted (A={per_a_nnls:.2f}, B={per_b_nnls:.2f}, C={per_c_nnls:.2f}, D={per_d:.2f})', line=dict(color='blue', width=2)), row=1, col=1)
 
     # Residuals in second row
-    fig.add_trace(go.Scatter(x=x, y=y_residual, mode='lines', name='Residuals'), row=2, col=1)
+    fig.add_trace(go.Scatter(x=common_x, y=y_residual, mode='lines', name='Residuals',line=dict(color='steelblue')), row=2, col=1)
     # Residuals in second row
-    fig.add_trace(go.Scatter(x=x, y=y_residual_fit, mode='lines', name='Residuals-Fit'), row=2, col=1)
+    fig.add_trace(go.Scatter(x=common_x, y=y_residual_fit, mode='lines', name='Residuals-Fit',line=dict(color='red')), row=2, col=1)
 
 
     # Update layout
